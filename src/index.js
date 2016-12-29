b2be5f9036520dc3f7bfd7c6db5397230665bd75
Oreoluwa <oreoluwa.banwo@andela.com>
2016-12-29 22:30:34 +0100

    feature: implement methods for string class 2

--- src/index.js
+++ src/index.js
@@ -1,142 +1,204 @@
 
 const extendStringClass = {
-  
+  /**
+   * Returns true if the string contains vowels.
+   * @return {Boolean}
+   */
   hasVowels() {
-   return /[aeiou]/.test(this);
+    return /[aeiou]/.test(this);
   },
-  
-//   Methods
-// hasVowels (typeof Boolean): Returns true if the string contains vowels. This method must implement Regular Expression.
 
-// toUpper (typeof String): Returns the String in question but with all characters in upper cases as applicable. This method should be implemented without the use of the toUpperCase internal methods.
+  /**
+   * Returns the String in question but with all characters in upper case.
+   * @return {String}
+   */
+
   toUpper() {
-    let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let charactersar = characters.split('');
-    return this.replace(/[a-z]/g, function(m){
-     return charactersar[charactersar.indexOf(m)+ 26];
-   });
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const charactersar = characters.split('');
+    return this.replace(/[a-z]/g, (m) => charactersar[charactersar.indexOf(m) + 26]);
   },
 
-// toLower (typeof String): Returns the String in question but with all characters in their lower cases as applicable. This method should be implemented without the use of the toLowerCase internal methods.
+  /**
+   * Returns the String in question but with all characters in lower case.
+   * @return {String}
+   */
   toLower() {
-    let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let charactersar = characters.split('');
-    return this.replace(/[A-Z]/g, function(m){
-     return charactersar[charactersar.indexOf(m)- 26];
-   });
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const charactersar = characters.split('');
+    return this.replace(/[A-Z]/g, (m) => charactersar[charactersar.indexOf(m) - 26]);
   },
-// ucFirst (typeof String): Returns the String in question but changes the First Character to an Upper case. Make use of your toUpper method above to implement this method or a one-liner.
-ucFirst() {
-    return this.replace(/[A-Z]/i, function(m){
-     return m.toUpper();
-   });
+  /**
+   * Returns the String in question but with the first characters in upper case.
+   * @return {String}
+   */
+  ucFirst() {
+    return this.replace(/[A-Z]/i, (m) => m.toUpper());
   },
-
-// isQuestion (typeof Boolean): Return true if the string is a question (ending with a question mark). This method must implement Regular Expression.
-isQuestion() {
+  /**
+   * Return true if the string is a question.
+   * @return {String}
+   */
+  isQuestion() {
     return /\?$/.test(this);
   },
-// words (typeof Object, instanceof Array): Returns a list of the words in the string, as an Array. This method must implement Regular Expression.
-words() {
-  return this.match(/\w+/g);
+  /**
+   * Returns a list of the words in the string
+   * @return {String};
+   */
 
-},
+  words() {
+    return this.match(/\w+/g);
 
-// wordCount (typeof Number): Returns the number of words in the string. It must make use of the words method above.
-wordCount() {
-  return this.words().length;
-},
-// toCurrency (typeof String): Returns a currency representation of the String e.g 11111.11 should be represented as 11,111.11.
-toCurrency() {
-   if (/\.\d\d$/.test(this)){
-      return this.replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
-    }else {
-      let thiss = `${this}.00`;
-   return thiss.replace(/(\d)(?=(\d{3})+\.)/g, '$1,'); 
-    }
-},
-// fromCurrency (typeof Number): Returns a number representation of the Currency String e.g 11,111.11 should return 11111.11
-fromCurrency() {
-   return parseFloat(this.replace(/,/g, ''));
   },
-// inverseCase (typeof String): Returns each letter in the string as an inverse of its current case e.g Mr. Ben should return mR. bEN.
-inverseCase() {
-    return this.replace(/[a-zA-Z]/g, function(m) {
-      if (/[a-z]/.test(m)){
+  /**
+   * Returns the number of words in the string
+   * @return {Number}
+   */
+  wordCount() {
+    return this.words().length;
+  },
+  /**
+   * Returns a currency representation of the String
+   * @return {String}
+   */
+  toCurrency() {
+    if (/\.\d\d$/.test(this)) {
+      return this.replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+    } else {
+      const thiss = `${this}.00`;
+      return thiss.replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+    }
+  },
+
+  /**
+   * Returns a number representation of the Currency String
+   * @return {Number}
+   */
+  fromCurrency() {
+    return parseFloat(this.replace(/,/g, ''));
+  },
+  /**
+   * Returns each letter as an inverse of its current case
+   * @return {String}
+   */
+  inverseCase() {
+    return this.replace(/[a-zA-Z]/g, (m) => {
+      if (/[a-z]/.test(m)) {
         return m.toUpper();
       } else {
         return m.toLower();
       }
-   });
+    });
   },
-// alternatingCase (typeof String): Returns the letters in alternating cases. It must start with a lower case e.g Onomatopoeia should return oNoMaToPoEiA.
-alternatingCase() {
-  if(/[^a-zA-Z]/.test(this)) {
-    return 'Invalid String';
-  }
-  let lower = true;
-  return this.replace(/[a-zA-Z]/g, function (m) {
-    if (lower){
-      lower = !lower;
-      return m.toLower();
-    } else {
-      lower = !lower;
-      return m.toUpper();
+  /**
+   * Returns the letters in alternating case
+   * @return {String}
+   */
+  alternatingCase() {
+    if (/[^a-zA-Z]/.test(this)) {
+      return 'Invalid String';
     }
-  });
-},
-// getMiddle (typeof String): Returns the character(s) in the middle of the string e.g read should return ea and reads should return a.
-getMiddle() {
-  if (this.length % 2 === 0) {
-    const middle = [this.length/2, (this.length/2) - 1]
-    let count = 0
-    return this.replace(/./g, function (m) {
-      if (count === middle[0] || count === middle[1]) {
-        count += 1;
-        return m;
+    let lower = true;
+    return this.replace(/[a-zA-Z]/g, (m) => {
+      if (lower) {
+        lower = !lower;
+        return m.toLower();
       } else {
-        count += 1;
-        return '';
+        lower = !lower;
+        return m.toUpper();
       }
-    })
-  } else {
-    const middle = Math.floor(this.length/2)
-    let count = 0
-    return this.replace(/./g, function (m) {
-      if (count === middle) {
-        count += 1;
-        return m;
+    });
+  },
+  /**
+   * Returns the character(s) in the middle of the string
+   * @return {String}
+   */
+  getMiddle() {
+    if (this.length % 2 === 0) {
+      const middle = [this.length / 2, (this.length / 2) - 1];
+      let count = 0;
+      return this.replace(/./g, (m) => {
+        if (count === middle[0] || count === middle[1]) {
+          count += 1;
+          return m;
+        } else {
+          count += 1;
+          return '';
+        }
+      });
+    } else {
+      const middle = Math.floor(this.length / 2);
+      let count = 0;
+      return this.replace(/./g, (m) => {
+        if (count === middle) {
+          count += 1;
+          return m;
+        } else {
+          count += 1;
+          return '';
+        }
+      });
+    }
+  },
+  /**
+   * Returns the numbers in words
+   * @return {String}
+   */
+  numberWords() {
+    if (/[a-zA-Z]/.test(this)) {
+      return 'Invalid String';
+    }
+    const matchTable = {
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine',
+      0: 'zero'
+    };
+    let count = 1;
+    const thiss = this;
+    return this.replace(/[0-9]/g, (m) => {
+      if (thiss.length > 1) {
+        if (count === thiss.length) {
+          return `${matchTable[m]}`;
+        } else {
+          count += 1;
+          return `${matchTable[m]} `;
+        }
       } else {
-        count += 1;
-        return '';
+        return matchTable[m];
       }
-    })
+
+    });
+  },
+
+/**
+ * Returns true if the string is a digit
+ * @return {Boolean}
+ */
+  isDigit() {
+    if (/\d?/.test(this)) {
+      if (this.length > 1) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  },
+  /**
+   * Returns true if a string contains double characters
+   * @return {Boolean}
+   */
+  doubleCheck() {
+    return /(.)\1/g.test(this);
   }
-},
-// numberWords (typeof String): Returns the numbers in words e.g 325 should return three two five.
-numberWords() {
-  const matchTable = {
-    1:'one ',
-    2:'two ',
-    3:'three ',
-    4:'four ',
-    5:'five ',
-    6:'six ',
-    7:'seven ',
-    8:'eight ',
-    9:'nine ',
-    0:'zero '
-  }
-  return this.replace(/[0-9]/g, function (m) {
-    return matchTable.m;
-  });
-}
 
-// isDigit (typeof Boolean): Returns true if the string is a digit(one number) e.g 3 should return true and 34 should return false. This method must implement Regular Expression.
-
-// doubleCheck (typeof Boolean): Returns true if a string contains double characters(including whitespace character) e.g aa, !!, should return true.
-
-  
 };
 
 Object.assign(String.prototype, extendStringClass);
--- tests/tests.js
+++ tests/tests.js
@@ -1,108 +1,101 @@
-
-// Methods
-// hasVowels (typeof Boolean): Returns true if the string contains vowels. This method must implement Regular Expression.
-describe("String Class Extra Methods", () => {
-  it("Should return True if the string contains vowels", () => {
+describe('String Class Extra Methods', () => {
+  it('Should return True if the string contains vowels', () => {
     expect('Testing'.hasVowels()).toEqual(true);
     expect('tstng'.hasVowels()).toEqual(false);
   });
 
-  // toUpper (typeof String): Returns the String in question but with all characters in upper cases as applicable. This method should be implemented without the use of the toUpperCase internal methods.
-  it("Should return uppercase string for all passed in data", () => {
-    expect('Testing'.toUpper()).toEqual("TESTING");
-    expect('tsTing'.toUpper()).toEqual("TSTING");
-    expect('JINGLE'.toUpper()).toEqual("JINGLE");
-    expect('jingl55E'.toUpper()).toEqual("JINGL55E");
-
+  it('Should return uppercase string for all passed in data', () => {
+    expect('Testing'.toUpper()).toEqual('TESTING');
+    expect('tsTing'.toUpper()).toEqual('TSTING');
+    expect('JINGLE'.toUpper()).toEqual('JINGLE');
+    expect('jingl55E'.toUpper()).toEqual('JINGL55E');
   });
 
-  // toLower (typeof String): Returns the String in question but with all characters in their lower cases as applicable. This method should be implemented without the use of the toLowerCase internal methods.
-  it("Should return lowercase string for all passed in strings", () => {
-    expect('testing'.toLower()).toEqual("testing");
-    expect('tsTing'.toLower()).toEqual("tsting");
-    expect('JINGLE'.toLower()).toEqual("jingle");
-    expect('jiNgl55E'.toLower()).toEqual("jingl55e");
+  it('Should return lowercase string for all passed in strings', () => {
+    expect('testing'.toLower()).toEqual('testing');
+    expect('tsTing'.toLower()).toEqual('tsting');
+    expect('JINGLE'.toLower()).toEqual('jingle');
+    expect('jiNgl55E'.toLower()).toEqual('jingl55e');
   });
-  // ucFirst (typeof String): Returns the String in question but changes the First Character to an Upper case. Make use of your toUpper method above to implement this method or a one-liner.
-  it("Should return string with first character in uppercase", () => {
-    expect('Testing'.ucFirst()).toEqual("Testing");
-    expect('tsTing'.ucFirst()).toEqual("TsTing");
-    expect('jingle'.ucFirst()).toEqual("Jingle");
+
+  it('Should return string with first character in uppercase', () => {
+    expect('Testing'.ucFirst()).toEqual('Testing');
+    expect('tsTing'.ucFirst()).toEqual('TsTing');
+    expect('jingle'.ucFirst()).toEqual('Jingle');
   });
-  // isQuestion (typeof Boolean): Return true if the string is a question (ending with a question mark). This method must implement Regular Expression.
-  it("Should return true if a string is a question", () => {
+
+  it('Should return true if a string is a question', () => {
     expect('Testing?'.isQuestion()).toEqual(true);
     expect('tsTing isn\'t it?'.isQuestion()).toEqual(true);
     expect('JINGLE'.isQuestion()).toEqual(false);
   });
-  // words (typeof Object, instanceof Array): Returns a list of the words in the string, as an Array. This method must implement Regular Expression.
-     const wordsResult1 = ['Testing', 'of', 'the', 'properties'];
-     const wordsResult2 = ['tsTing', 'of', 'the', 'world', 'is'];
-     const wordsResult3 = ['JINGLe'];
-  it("Should return an array of the words contained in inputed string.", () => {
+
+  const wordsResult1 = ['Testing', 'of', 'the', 'properties'];
+  const wordsResult2 = ['tsTing', 'of', 'the', 'world', 'is'];
+  const wordsResult3 = ['JINGLe'];
+  it('Should return an array of the words contained in inputed string.', () => {
     expect('Testing of the properties '.words()).toEqual(wordsResult1);
     expect('tsTing of the world is'.words()).toEqual(wordsResult2);
     expect('JINGLe'.words()).toEqual(wordsResult3);
   });
-  // wordCount (typeof Number): Returns the number of words in the string. It must make use of the words method above.
-  it("Should return count of words contained in entered string", () => {
+
+  it('Should return count of words contained in entered string', () => {
     expect('Testing of the properties '.wordCount()).toEqual(4);
     expect('tsTing of the world is'.wordCount()).toEqual(5);
     expect('JINGLe'.wordCount()).toEqual(1);
-    expect(typeof('tsing is the world'.wordCount())).toEqual('number')
+    expect(typeof ('tsing is the world'.wordCount())).toEqual('number');
   });
-  // toCurrency (typeof String): Returns a currency representation of the String e.g 11111.11 should be represented as 11,111.11.
-  it("Should return a currency representation of the String", () => {
+
+  it('Should return a currency representation of the String', () => {
     expect('111111.11'.toCurrency()).toEqual('111,111.11');
     expect('100255452875.23'.toCurrency()).toEqual('100,255,452,875.23');
     expect('5006254'.toCurrency()).toEqual('5,006,254.00');
   });
-  // fromCurrency (typeof Number): Returns a number representation of the Currency String e.g 11,111.11 should return 11111.11
-  it("Should return a string representation of the inputed currency", () => {
+
+  it('Should return a string representation of the inputed currency', () => {
     expect('111,111.11'.fromCurrency()).toEqual(111111.11);
     expect('100,255,452,875.23'.fromCurrency()).toEqual(100255452875.23);
     expect('5,006,254.00'.fromCurrency()).toEqual(5006254.00);
     expect('5,006,254.00'.fromCurrency()).toEqual(5006254.00);
-    expect(typeof('3,555,33.00'.fromCurrency())).toEqual('number');
+    expect(typeof ('3,555,33.00'.fromCurrency())).toEqual('number');
   });
-  // inverseCase (typeof String): Returns each letter in the string as an inverse of its current case e.g Mr. Ben should return mR. bEN.
-  it("Should return each letter in the string as an inverse of its current case", () => {
+
+  it('Should return each letter as an inverse of its current case', () => {
     expect('HALLELUYAH'.inverseCase()).toEqual('halleluyah');
     expect('halleluyah'.inverseCase()).toEqual('HALLELUYAH');
     expect('halleLUYah'.inverseCase()).toEqual('HALLEluyAH');
     expect('5,006,gfhYY254.00'.inverseCase()).toEqual('5,006,GFHyy254.00');
   });
-  // alternatingCase (typeof String): Returns the letters in alternating cases. It must start with a lower case e.g Onomatopoeia should return oNoMaToPoEiA.
-  it("Should return the letters in alternating case", () => {
+
+  it('Should return the letters in alternating case', () => {
     expect('HALLELUYAH'.alternatingCase()).toEqual('hAlLeLuYaH');
     expect('halleluyah'.alternatingCase()).toEqual('hAlLeLuYaH');
     expect('halleLUYah'.alternatingCase()).toEqual('hAlLeLuYaH');
     expect('5,006,254.00'.alternatingCase()).toEqual('Invalid String');
   });
-  // getMiddle (typeof String): Returns the character(s) in the middle of the string e.g read should return ea and reads should return a.
-  it("Should return the character(s) in the middle of the string", () => {
+
+  it('Should return the character(s) in the middle of the string', () => {
     expect('HALLELUYAH'.getMiddle()).toEqual('EL');
     expect('halleluyahs'.getMiddle()).toEqual('l');
     expect('halleLUYah'.getMiddle()).toEqual('eL');
     expect('5,006,254.00'.getMiddle()).toEqual(',2');
   });
-  // numberWords (typeof String): Returns the numbers in words e.g 325 should return three two five.
-  it("Should return the numbers in words", () => {
+
+  it('Should return the numbers in words', () => {
     expect('HALLELUYAH'.numberWords()).toEqual('Invalid String');
     expect('3'.numberWords()).toEqual('three');
     expect('54'.numberWords()).toEqual('five four');
     expect('000'.numberWords()).toEqual('zero zero zero');
   });
-  // isDigit (typeof Boolean): Returns true if the string is a digit(one number) e.g 3 should return true and 34 should return false. This method must implement Regular Expression.
-  it("Should return true if the string is a single digit", () => {
-    expect('HALLELUYAH'.numberWords()).toEqual(false);
-    expect('3'.numberWords()).toEqual(true);
-    expect('54'.numberWords()).toEqual(false);
-    expect('0'.numberWords()).toEqual(true);
+
+  it('Should return true if the string is a single digit', () => {
+    expect('HALLELUYAH'.isDigit()).toEqual(false);
+    expect('3'.isDigit()).toEqual(true);
+    expect('54'.isDigit()).toEqual(false);
+    expect('0'.isDigit()).toEqual(true);
   });
 
-  // doubleCheck (typeof Boolean): Returns true if a string contains double characters(including whitespace character) e.g aa, !!, should return true.
-  it("Should return true if a string contains double characters", () => {
+  it('Should return true if a string contains double characters', () => {
     expect('HALLELUYAH'.doubleCheck()).toEqual(true);
     expect('3'.doubleCheck()).toEqual(false);
     expect('5477'.doubleCheck()).toEqual(true);
